ac0097aa4bb23479bcab2a8cb14f1aa4ada31046
Konstantin Kostadinov <47210793+Kalifer738@users.noreply.github.com>
2024-09-30 14:45:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
+
+const [count, setCount] = useState(0);
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div><span>Count: {count}</span><button onclick="click">Click me!</button></div>;
+  
+}
+
+function click() {
+  setCountFunc(() => {count + 1});
 }
 
 export default App;
